5a5ccbd64be1a42cefe8c677fb25349932feff0e
GANESHPRABHU078 <gp561910@gmail.com>
2026-04-05 18:28:14 +0530

    update

--- frontend/src/api/jobService.js
+++ frontend/src/api/jobService.js
@@ -1,22 +1,30 @@
-import api from './api';
+import api from './apiClient';
 
-export const getJobs = async (type = null) => {
-  const params = type ? { type } : {};
-  const response = await api.get('/jobs', { params });
-  return response.data;
+export const jobService = {
+  getAll: async (type = null) => {
+    const params = type ? { type } : {};
+    const response = await api.get('/jobs', { params });
+    return response.data;
+  },
+  
+  getNewJobs: async () => {
+    const response = await api.get('/jobs/new');
+    return response.data;
+  },
+  
+  getRecommendations: async () => {
+    const response = await api.get('/jobs/recommendations');
+    return response.data;
+  },
+  
+  createJob: async (jobData) => {
+    const response = await api.post('/jobs', jobData);
+    return response.data;
+  }
 };
 
-export const getNewJobs = async () => {
-  const response = await api.get('/jobs/new');
-  return response.data;
-};
-
-export const getRecommendations = async () => {
-  const response = await api.get('/jobs/recommendations');
-  return response.data;
-};
-
-export const createJob = async (jobData) => {
-  const response = await api.post('/jobs', jobData);
-  return response.data;
-};
+// Keep individual exports if needed elsewhere
+export const getJobs = jobService.getAll;
+export const getNewJobs = jobService.getNewJobs;
+export const getRecommendations = jobService.getRecommendations;
+export const createJob = jobService.createJob;
